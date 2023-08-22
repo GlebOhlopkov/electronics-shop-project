27752696a2841e254a55fbebfc14803ec6f2b3a0
glebokhlopkov <glebka_90@mail.ru>
2023-08-22 16:13:47 +0300

    done homework-1 unlock pytest in .gitignore install depends in .toml done item.py done test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture()
+def test_item():
+    return Item('TV', 5000, 10)
+
+
+def test_calculate_total_price(test_item):
+    assert test_item.calculate_total_price() == 50000
+
+
+def test_apply_discount(test_item):
+    test_item.pay_rate = 1.5
+    test_item.apply_discount()
+    assert test_item.price == 7500
